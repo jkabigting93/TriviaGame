8eeb7c0716c35ac55e4ee65cce639cd63505c19e
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-07-10 01:31:46 -0400

    hide game body at start

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
 
-}
+    // On-click listener for Start Button
+    $("#startButton").click(function() {
+        console.log("click works");
+
+    // Write game code to game area
+        // $("#game").html();
