8f9c06526bfe74b8800414f75ff0e24b5d4324e8
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-07-10 02:53:06 -0400

    wrote user input as array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,10 +25,7 @@ $(document).ready(function() {
         $("#scoreScreen").css("display", "block");
 
     // Submission Function - Define Answers as Array
-        var numQuestions = 10;
-        var numChoices = 4;
         var answers = new Array(10);
-
         answers[0] = "Toronto";
         answers[1] = "OVO";
         answers[2] = "bling";
@@ -40,5 +37,20 @@ $(document).ready(function() {
         answers[8] = "Fred Van Vleet";
         answers[9] = "Anchorman 2";
 
+    // Submission Function - Define Input as Array
+        var input = new Array(10);
+        input[0] = $("input[name='q1']:checked").val();
+        input[1] = $("input[name='q2']:checked").val();
+        input[2] = $("input[name='q3']:checked").val();
+        input[3] = $("input[name='q4']:checked").val();
+        input[4] = $("input[name='q5']:checked").val();
+        input[5] = $("input[name='q6']:checked").val();
+        input[6] = $("input[name='q7']:checked").val();
+        input[7] = $("input[name='q8']:checked").val();
+        input[8] = $("input[name='q9']:checked").val();
+        input[9] = $("input[name='q10']:checked").val();
+        console.log(input);
+
     });
+    
 });
