752f821c14a5365db461b0668960698839dac3ad
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-07-10 02:23:24 -0400

    got timer to work on start click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,15 +7,29 @@ $(document).ready(function() {
     // Display Game Body and hide Start Button
         $("#startButton").css("display", "none");
         $("#game").css("display", "block");
+
+    // Timer Function
+        var i = 120;
+        setInterval(function () {
+            $("#timeLeft").html(i);
+            i--;
+        }, 1000);
     });
 
     // On-click listener for Submission Button
     $("#submitButton").click(function() {
         console.log("submit click works");
+
+    // Display Score Screen and hide Game Body
         $("#game").css("display", "none");
         $("#scoreScreen").css("display", "block");
 
-    // Define Submission Function
-    
+    // Call Submission Function
+
     })
+
+    // Define Submission Function
+    // function submit() {
+    //     if
+    // }
 })
