57980a85a03d822027ed93dbb6cf2afa249e6dcb
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-07-10 02:05:58 -0400

    got submit button to work the same way as start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,20 @@ $(document).ready(function() {
 
     // On-click listener for Start Button
     $("#startButton").click(function() {
-        console.log("click works");
+        console.log("start click works");
 
     // Display Game Body and hide Start Button
         $("#startButton").css("display", "none");
         $("#game").css("display", "block");
     });
 
+    // On-click listener for Submission Button
+    $("#submitButton").click(function() {
+        console.log("submit click works");
+        $("#game").css("display", "none");
+        $("#scoreScreen").css("display", "block");
+
     // Define Submission Function
+    
+    })
 })
